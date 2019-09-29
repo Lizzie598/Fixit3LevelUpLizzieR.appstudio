7cba363b5c4622c29870fb21ba50031ffa84e7c9
Lizzie <ekr74869@creighton.edu>
2019-09-29 15:01:35 -0500

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -4,3 +4,15 @@ function carLoan(loanTotal, timePeriod, interestRate) {
   let monthlyPayment = loanTotal*((interestPerMonth*interestTime)/(interestTime - 1))
   return monthlyPayment
 }
+
+/*
+//main program
+let calculator = prompt("Which calculator would you like to use?")
+*/
+
+//main program
+let loanTotal = prompt("How much is your loan?")
+let timePeriod = prompt("How many months is your loan for?")
+let interestRate = prompt("What is your interest rate? (decimal form only)")
+let myCarLoan = carLoan(loanTotal, timePeriod, interestRate)
+alert(`A car loan for ${loanTotal} over ${timePeriod} months at ${interestRate} interest would have a monthly payment of ${monthlyPayment}`)
