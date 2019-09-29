24c71bc84a2d42456be74b8cd21a39fcd5a7f3cd
Lizzie <ekr74869@creighton.edu>
2019-09-29 15:33:48 -0500

    Home Changes

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,33 @@
+function carLoan(loanTotal, timePeriod, interestRate) {
+  let interestPerMonth = Number(interestRate/12)
+  let interestTime = Number((interestPerMonth + 1)**timePeriod)
+  let monthlyPayment = loanTotal*((interestPerMonth*interestTime)/(interestTime - 1))
+  return monthlyPayment
+}
+
+function homeLoan(loanAmount, timeLength, interestRateH) {
+  let interestRatePerMonth = Number(interestRateH/12)
+  let interestTimeLength = Number((interestRatePerMonth + 1)**timeLength)
+  let monthlyPaymentHouse = loanTotal*((interestPerMonth*interestTime)/(interestTime - 1))
+  return monthlyPaymentHouse
+}
+
+/*
+//main program
+let calculator = prompt("Which calculator would you like to use?")
+*/
+
+//main program
+let loanTotal = prompt("How much is your loan?")
+let timePeriod = prompt("How many months is your loan for?")
+let interestRate = prompt("What is your interest rate? (decimal form only)")
+let myCarLoan = carLoan(loanTotal, timePeriod, interestRate)
+alert(`A car loan for ${loanTotal} over ${timePeriod} months at ${interestRate} interest would have a monthly payment of ${myCarLoan}`)
+
+var i = 0;
+do {
+  myCarLoan = carLoan(loanTotal, timePeriod, interestRate);
+  alert(`A car loan for ${loanTotal} over ${timePeriod} months at ${interestRate} interest would have a monthly payment of ${myCarLoan}`);
+}
+  while(myCarLoan[i] !== 0);
+    
